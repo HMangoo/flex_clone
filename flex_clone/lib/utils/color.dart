231e82815a5dd99b076005bc170baefab7d5f600
HMangoo <osl40498208@gmail.com>
2022-07-25 15:24:33 +0900

    build

---
+++ flex_clone/lib/utils/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(25, 25, 25, 1);
+const baseColor1 = Color.fromRGBO(48, 48, 48, 1);
+const baseColor2 = Color.fromRGBO(60, 60, 60, 1);
+const baseColor3 = Color.fromRGBO(112, 112, 112, 1);
+const baseColor4 = Color.fromRGBO(187, 187, 187, 1);
+
+const answerColor = Color.fromRGBO(30, 69, 62, 1);
---
+++ flex_clone/lib/utils/font_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'color.dart';
+
+// all page (except signUpPage)
+const headLineStyle = TextStyle(
+  fontSize: 26,
+  color: backgroundColor,
+  fontWeight: FontWeight.bold,
+);
+const pageDescription = TextStyle(
+  color: baseColor4,
+  fontSize: 18,
+);
+
+// emblem, capacity
+const emblemFont = TextStyle(
+  color: baseColor4,
+  fontSize: 12,
+);
+const capacityFont = TextStyle(
+  color: backgroundColor,
+  fontSize: 18,
+);
+
+// location
+const districtFont = TextStyle(
+  color: backgroundColor,
+  fontSize: 20,
+);
+const cityFont = TextStyle(
+  color: backgroundColor,
+  fontSize: 18,
+);
